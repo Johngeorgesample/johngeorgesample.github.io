40d396b5d83f443c60d90005ce815fc8e6ae2232
John-George Sample <johngeorgesample@gmail.com>
2016-06-10 14:37:48 -0400

    .

---
+++ test.js
@@ -0,0 +1,28 @@
+/*
+stabby.js™ is the brainchild of SRU's Web Communications. This framework is going to revolutionize the Web industry one user at a time. stabby.js™ is one of multiple intended frameworks for the SUX™ (Surprise User eXperience™) developmental concept. 
+*/
+
+function stabUser() {
+	console.log("Stab!");
+}
+
+//Don't (do) use this cause document.write "sux"!
+function stabUserPage() {
+	document.write("Stab!");
+}
+
+function stabFrenzy(){
+	for(i = 0; i <= 100; i++){
+		console.log("Stab Frenzy!");	
+	}
+}
+
+function buttonStab() {
+	console.log("🔪");
+}
+
+function changeText() {
+	var stabReplace = "STAB";
+	var id = document.getElementById("stabText");
+	id.innerHTML=stabReplace;
+}
